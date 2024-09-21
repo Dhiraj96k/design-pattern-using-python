2a6b22ef9d1890a691259ecd566ef6deeb5356c4
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-09-21 23:54:14 +0530

    addition of number until its sum not become single digite

--- pattern2.py
+++ pattern2.py
@@ -2,10 +2,11 @@ n = int(input("enter number"))
 res = 0
 temp=11
 
-while temp >10:
+while n >10 :
     while n > 0:
         i = n%10 #return last digite
-        res=res+i
-        temp=res    
+        res=res+i   
         n=n//10 # remove last digite
-print(res)
+    n=res
+    res=0
+print(n)
