392efae4e5d8ae07f8afdbac60e344deeb53a20a
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-09-22 21:31:50 +0530

    that code give the prime number in starting from ending

--- pattern3.py
+++ pattern3.py
@@ -1,5 +1,10 @@
-start = 1
-end = 100
-
-for i in range(1,end):
-    
+s = int(input("Enter start number: "))
+n = int(input("Enter end number: "))
+for i in range(s, n):
+    if i < 2:
+        continue  
+    for j in range(2, i):  
+        if i % j == 0:
+            break 
+    else:
+        print(i, end=" ")  # Prime number found
