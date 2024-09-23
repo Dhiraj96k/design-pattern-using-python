ce24d0d06ce08f3c64f121f0d5374345ae1ad95c
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-09-23 21:35:29 +0530

    prime  number in specific range from starting to end

--- pattern3.py
+++ pattern3.py
@@ -2,7 +2,8 @@ s = int(input("Enter start number: "))
 n = int(input("Enter end number: "))
 for i in range(s, n):
     if i < 2:
-        continue  
+        continue 
+     
     for j in range(2, i):  
         if i % j == 0:
             break 
