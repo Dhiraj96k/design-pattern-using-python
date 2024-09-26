79427fd11b2d7e74a730a343a59f1c05be21ddb8
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-09-26 22:57:59 +0530

    Number Pattern Mirrored Semi-Pyramid

--- pattern6.py
+++ pattern6.py
@@ -4,6 +4,7 @@ rows = int(input("Enter the number of rows: "))
 for i in range(1, rows+1):  
     num = 1
     #Inner for loop to handle number of columns  
+
     #values change according to the outer loop  
     for j in range(rows,0,-1):  
       if j > i:
@@ -11,6 +12,6 @@ for i in range(1, rows+1):
       else:
         print(num, end=" ")
         num += 1
-  
+
     #End line after each row  
     print()
