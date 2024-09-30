8d4baa0a4d60e2bbf2d060dbfb332f920d66c324
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-09-30 22:29:41 +0530

    number semi-pyramid

--- pattern9.py
+++ pattern9.py
@@ -1,7 +1,6 @@
 # Ask the user for the number of rows they want in the pattern
 rows = int(input("Enter the number of rows: "))  
 
-# Loop through each row from 1 to the number of rows entered by the user
 for i in range(1, rows + 1):  
 # For each row, print numbers starting from 1 up to the current row number
     for j in range(1, i + 1):  
