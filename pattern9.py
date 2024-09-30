044ff34493a43ff7b6d7080641b18005885ae8c6
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-09-30 22:28:19 +0530

    number semi pyramid

--- pattern9.py
+++ pattern9.py
@@ -3,10 +3,10 @@ rows = int(input("Enter the number of rows: "))
 
 # Loop through each row from 1 to the number of rows entered by the user
 for i in range(1, rows + 1):  
-    # For each row, print numbers starting from 1 up to the current row number
+# For each row, print numbers starting from 1 up to the current row number
     for j in range(1, i + 1):  
-        # Print the current number and stay on the same line
+# Print the current number and stay on the same line
         print(j, end=" ")       
   
-    # Move to the next line after finishing the numbers for the current row
+# Move to the next line after finishing the numbers for the current row
     print()
