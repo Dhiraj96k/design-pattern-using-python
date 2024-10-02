eca5b8a1ce6c28364e8fff0566d11349273db8dd
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-10-02 22:02:10 +0530

    integer number to Roman Number Conversion

--- pattern11.py
+++ pattern11.py
@@ -2,3 +2,8 @@ value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 5, 4, 1]
 num = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "V", "IV", "I"]
 n = int(input("Enter Number: "))
 result = ""
+
+for i in range(len(value)):
+    while n >= value[i]:
+        result += num[i]  
+print(result)
