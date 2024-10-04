1751a700c8626f2cf626d18a6e0516a5c56132b7
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-10-04 23:06:16 +0530

    pascals shape pyrmid

--- pattern12.py
+++ pattern12.py
@@ -7,4 +7,7 @@ for i in range(1, rows+1):
     for j in range(0, i):
         if j==0 or i==0:
             coef = 1
-       
+        else:
+            coef = coef * (i - j)//j
+        print(coef, end = " ")
+    print()
