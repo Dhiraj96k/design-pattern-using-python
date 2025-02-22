325bf77e170514537e732c23577bc5ecd086d89d
Dhiraj Dhanaji Salunke <168630573+Dhiraj96k@users.noreply.github.com>
2025-02-22 23:19:34 +0530

    Update pattern6.py

--- pattern6.py
+++ pattern6.py
@@ -4,7 +4,6 @@ rows = int(input("Enter the number of rows: "))
 for i in range(1, rows+1):  
     num = 1
     #Inner for loop to handle number of columns  
-
     #values change according to the outer loop  
     for j in range(rows,0,-1):  
       if j > i:
